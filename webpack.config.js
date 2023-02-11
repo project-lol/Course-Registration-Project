37fe62f5c54d3b1ff61d14ed78c07128bff9175d
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-02-11 17:06:45 +0900

    feat : code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const CopyPlugin = require("copy-webpack-plugin")
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer")
 
 module.exports = {
   entry: {
@@ -87,5 +88,11 @@ module.exports = {
         },
       ],
     }),
+    new BundleAnalyzerPlugin({}),
   ],
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
 }
